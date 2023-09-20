70e16d382a64f268b0f553d5555c41c6750eda61
Brad <brad@4pi.com.au>
2023-09-20 20:15:44 +1000

    Very basic WIP

---
+++ package/WingetManifestBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using Octokit;
+using System.Diagnostics;
+
+var client = new GitHubClient(new ProductHeaderValue("DLSS-Swapper_WingetManifestBuilder"));
+var releases = await client.Repository.Release.GetAll("beeradmoore", "dlss-swapper");
+var latest = releases.FirstOrDefault();
+
+Debugger.Break();
+
+Console.WriteLine(
+    "The latest release is tagged at {0} and is named {1}",
+    latest.TagName,
+    latest.Name);
